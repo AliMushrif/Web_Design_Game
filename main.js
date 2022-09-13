830731b0e7c550ee94ade803f56c5092870522c2
AliMushrif96 <ali.mushrif996@gmail.com>
2022-09-13 03:26:52 +0300

    the final Virsion

--- main.js
+++ main.js
@@ -12,7 +12,12 @@ searchBtn.onclick = function () {
 //
 
 let time = 0;
-const paragraphs = ["First", "Second", "Third", "Fourth"];
+const paragraphs = [
+  "Hello, now we will talk about the css flexbox how to run it",
+  "When you write the (display: flex;) you will start the flexbox",
+  "Flexbox makes the elements inside the parents responsive, and you can modify eazly",
+  "we make the selection for the element with (.) at start of the name because it's class",
+];
 
 function changeText() {
   let text = `${paragraphs[time]} Paragraph`;
@@ -27,29 +32,29 @@ function changeText() {
   }
 }
 //
-// let colCon = document.querySelector(".columnContanter");
-// let cssCode = document.getElementById("csscode");
-// let cssCodeRg = ".alignCulomn{display: flex;}";
-// let submitbtn = document.querySelector(".reg");
+let colCon = document.querySelector(".columnContanter");
+let cssCode = document.getElementById("csscode");
+let cssCodeRg = ".alignCulomn{display: flex;}";
+let submitbtn = document.querySelector(".reg");
 
-// submitbtn.addEventListener("click", () => {
-//   cssCode = cssCode.value;
-//   if (cssCodeRg == cssCode) {
-//     colCon.style.display = "flex";
-//   } else {
-//     document.getElementById("csscode").value = "wrong";
-//   }
-// });
-//
-document.getElementById("codeAnwsoer").onsubmit = function (e) {
-  let colCon = document.querySelector(".columnContanter");
-  let cssCode = document.getElementById("csscode").value;
-  let cssCodeRg = /.alignCulomn{display: flex;}/gi;
-  let applyCode = cssCode.match(cssCodeRg);
-  if (applyCode) {
+submitbtn.addEventListener("click", () => {
+  cssCode = cssCode.value;
+  if (cssCodeRg == cssCode) {
     colCon.style.display = "flex";
   } else {
     document.getElementById("csscode").value = "wrong";
   }
-  e.preventDefault();
-};
+});
+//
+// document.getElementById("codeAnwsoer").onsubmit = function (e) {
+//   let colCon = document.querySelector(".columnContanter");
+//   let cssCode = document.getElementById("csscode").value;
+//   let cssCodeRg = /.alignCulomn{display: flex;}/gi;
+//   let applyCode = cssCode.match(cssCodeRg);
+//   if (applyCode) {
+//     colCon.style.display = "flex";
+//   } else {
+//     document.getElementById("csscode").value = "wrong";
+//   }
+//   e.preventDefault();
+// };
